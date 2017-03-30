506da73c4d0400f82d7e494c36578c030acafb25
mjmartinez <mjmartinez@optimopg.com>
2017-03-30 11:09:04 -0300

    El sitio muestra el listado de empresas

--- src/EmpresasBundle/Controller/DefaultController.php
+++ src/EmpresasBundle/Controller/DefaultController.php
@@ -12,6 +12,31 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('EmpresasBundle:Default:index.html.twig');
+        $empresas= $this->getDoctrine()->getRepository('EmpresasBundle:Empresas')->findAll();
+        return $this->render('EmpresasBundle:Default:index.html.twig', array('empresas' =>$empresas));
+    }
+ 
+
+    /**
+     * @Route("/agregar")
+     */
+    public function agregarAction(Request $request)
+    {
+        return $this->render('abm/agregar.html.twig');
+    }
+
+    /**
+     * @Route("/eliminar/{id}")
+     */
+    public function eliminarAction($id,Request $request)
+    {
+        return $this->render('abm/eliminar.html.twig');
+    }
+    /**
+     * @Route("/editar/{id}")
+     */
+    public function editarAction($id,Request $request)
+    {
+        return $this->render('abm/editar.html.twig');
     }
 }
